7403779b22e2c3407ceee6861ec29bf87705fe5d
Connor Boyle <cjblink1@gmail.com>
2019-05-15 17:02:24 +0000

    Convert to GET request

--- src/app/management/common/list-games/list-games.service.ts
+++ src/app/management/common/list-games/list-games.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Game} from "../game/game";
-import {HttpClient, HttpRequest, HttpResponse} from "@angular/common/http";
+import {HttpClient, HttpParams, HttpRequest, HttpResponse} from "@angular/common/http";
 
 class ListGamesResponse {
   data: Game[];
@@ -15,14 +15,16 @@ export class ListGamesService {
 
   listGames(): Promise<Game[]> {
     return new Promise<Game[]>((resolve, reject) => {
-      this.http.post<ListGamesResponse>('/api/listGames', '{ "fields": [] }')
-        .subscribe(response => {
+      this.http.get<ListGamesResponse>('/api/listGames', {
+        params: new HttpParams()
+          .set('fields', 'gameID,name')
+      }).subscribe(response => {
           if (! response.success) {
             reject(response.error);
             return;
           }
           resolve(response.data);
-        });
+      })
     });
   }
 }
