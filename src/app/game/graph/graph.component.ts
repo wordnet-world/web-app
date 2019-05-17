ae76fe0c02bfc82fb93d690c0ec239348a6c85e5
Connor Boyle <cjblink1@gmail.com>
2019-05-17 04:52:30 +0000

    More refactoring

--- src/app/game/graph/graph.component.ts
+++ src/app/game/graph/graph.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as d3 from 'd3';
+import {GameService, Graph} from "../game/game.service";
 
 @Component({
   selector: 'app-graph',
@@ -13,33 +14,15 @@ export class GraphComponent implements OnInit {
   private zoom;
   private currentSelected;
   private currentSelectedData;
+  private node;
+  private link;
+  private label;
+  private simulation;
+  private color;
 
-  private graph: any =
-    {
-      "nodes": [
-        {"id": "Me", "group": 1},
-        {"id": "Friend1", "group": 2},
-        {"id": "Friend2", "group": 2},
-        {"id": "Friend3", "group": 2},
-        {"id": "Friend4", "group": 2},
-        {"id": "Friend5", "group": 2},
-        {"id": "Friend6", "group": 2},
-        {"id": "Friend7", "group": 2},
-        {"id": "Friend8", "group": 2}
-      ],
-      "links": [
-        {"source": "Me", "target": "Friend1", "value": 1},
-        {"source": "Me", "target": "Friend2", "value": 1},
-        {"source": "Me", "target": "Friend3", "value": 5},
-        {"source": "Me", "target": "Friend4", "value": 1},
-        {"source": "Me", "target": "Friend5", "value": 1},
-        {"source": "Me", "target": "Friend6", "value": 1},
-        {"source": "Me", "target": "Friend7", "value": 1},
-        {"source": "Me", "target": "Friend8", "value": 1}
-      ]
-    };
-
-  constructor() { }
+  constructor(private gameService: GameService) {
+    this.gameService.graphSubject.subscribe(this.handleGraphUpdate);
+  }
 
   ngOnInit() {
 
@@ -47,7 +30,7 @@ export class GraphComponent implements OnInit {
     const width = parseInt(this.svg.style('width'), 10);
     const height = parseInt(this.svg.style('height'), 10);
 
-    const color = d3.scaleOrdinal(d3.schemeCategory10);
+    this.color = d3.scaleOrdinal(d3.schemeCategory10);
 
     const manyBodyForce = d3.forceManyBody();
     manyBodyForce.strength(-800);
@@ -55,7 +38,7 @@ export class GraphComponent implements OnInit {
     const linkForce = d3.forceLink().id(function (d: any) { return d.id; });
     linkForce.distance(200);
 
-    const simulation = d3.forceSimulation()
+    this.simulation = d3.forceSimulation()
       .force('collision', d3.forceCollide(40))
       .force('link', linkForce)
       .force('charge', manyBodyForce)
@@ -63,62 +46,6 @@ export class GraphComponent implements OnInit {
 
     this.g = this.svg.append('g').attr('class', 'everything');
 
-    const link = this.g.append('g')
-      .attr('class', 'links')
-      .selectAll('line')
-      .data(this.graph.links)
-      .enter().append('line')
-      .attr('stroke-width', 5);
-
-
-    const dragstarted = (d) => {
-      if (!d3.event.active) {
-        simulation.alphaTarget(.3).restart();
-      }
-      d.fx = d.x;
-      d.fy = d.y;
-    };
-
-    const dragged = (d) => {
-      d.fx = d3.event.x;
-      d.fy = d3.event.y;
-    };
-
-    const dragended = (d) => {
-      if (!d3.event.active) {
-        simulation.alphaTarget(0);
-      }
-      d.fx = null;
-      d.fy = null;
-    };
-
-    const clicked = (d, index, nodes) => {
-      d3.select(this.currentSelected).attr('class', '');
-      this.currentSelected = nodes[index];
-      this.currentSelectedData = d;
-      d3.select(this.currentSelected).attr('class', 'selected');
-    };
-
-    const node = this.g.append('g')
-      .attr('class', 'nodes')
-      .selectAll('circle')
-      .data(this.graph.nodes)
-      .enter().append('circle')
-      .attr('r', 30)
-      .attr('fill', function (d: any) { return color(d.group); })
-      .on('click', clicked)
-      .call(d3.drag()
-        .on('start', dragstarted)
-        .on('drag', dragged)
-        .on('end', dragended));
-
-    const label = this.g.append('g')
-      .attr('class', 'nodes')
-      .selectAll('circle')
-      .data(this.graph.nodes)
-      .enter().append('text')
-      .text(function (d: any) {return d.id})
-      .attr('text-anchor', 'middle');
 
     // Zoom functions
     const zoom_actions = () => {
@@ -133,33 +60,7 @@ export class GraphComponent implements OnInit {
 
     zoom_handler(this.svg);
 
-
-    node.append('title')
-      .text(function (d: any) { return d.id; });
-
-    simulation
-      .nodes(this.graph.nodes)
-      .on('tick', ticked);
-
-    simulation.force<any>('link')
-      .links(this.graph.links);
-
-
-    function ticked() {
-      link
-        .attr('x1', function (d: any) { return d.source.x; })
-        .attr('y1', function (d: any) { return d.source.y; })
-        .attr('x2', function (d: any) { return d.target.x; })
-        .attr('y2', function (d: any) { return d.target.y; });
-
-      node
-        .attr('cx', function (d: any) { return d.x; })
-        .attr('cy', function (d: any) { return d.y; });
-
-      label
-        .attr('x', function (d: any) { return d.x })
-        .attr('y', function (d: any) { return d.y });
-    }
+    this.restart({ "nodes": [], "edges": []});
   }
 
   handle(event) {
@@ -168,5 +69,91 @@ export class GraphComponent implements OnInit {
         this.svg.call(this.zoom.transform, d3.zoomIdentity);
       }
     }
+  }
+
+  handleGraphUpdate(graph: Graph) {
+    this.restart(graph);
+  }
+
+  restart(graph: Graph) {
+
+    const dragstarted = (d) => {
+      if (!d3.event.active) {
+        this.simulation.alphaTarget(.3).restart();
+      }
+      d.fx = d.x;
+      d.fy = d.y;
+    };
+
+    const dragged = (d) => {
+      d.fx = d3.event.x;
+      d.fy = d3.event.y;
+    };
+
+    const dragended = (d) => {
+      if (!d3.event.active) {
+        this.simulation.alphaTarget(0);
+      }
+      d.fx = null;
+      d.fy = null;
+    };
+
+    const clicked = (d, index, nodes) => {
+      d3.select(this.currentSelected).attr('class', '');
+      this.currentSelected = nodes[index];
+      this.currentSelectedData = d;
+      d3.select(this.currentSelected).attr('class', 'selected');
+    };
+
+    this.link = this.g.append('g')
+      .attr('class', 'links')
+      .selectAll('line')
+      .data(graph.edges)
+      .enter().append('line')
+      .attr('stroke-width', 5);
+
+    this.node = this.g.append('g')
+      .attr('class', 'nodes')
+      .selectAll('circle')
+      .data(graph.nodes)
+      .enter().append('circle')
+      .attr('r', 30)
+      .attr('fill',  (d: any) => { return this.color(d.group); })
+      .on('click', clicked)
+      .call(d3.drag()
+        .on('start', dragstarted)
+        .on('drag', dragged)
+        .on('end', dragended));
+
+    this.label = this.g.append('g')
+      .attr('class', 'nodes')
+      .selectAll('circle')
+      .data(graph.nodes)
+      .enter().append('text')
+      .text(function (d: any) {return d.id})
+      .attr('text-anchor', 'middle');
+
+    const ticked = () => {
+      this.link
+        .attr('x1', function (d: any) { return d.source.x; })
+        .attr('y1', function (d: any) { return d.source.y; })
+        .attr('x2', function (d: any) { return d.target.x; })
+        .attr('y2', function (d: any) { return d.target.y; });
+
+      this.node
+        .attr('cx', function (d: any) { return d.x; })
+        .attr('cy', function (d: any) { return d.y; });
+
+      this.label
+        .attr('x', function (d: any) { return d.x })
+        .attr('y', function (d: any) { return d.y });
+    };
+
+    this.simulation
+      .nodes(graph.nodes)
+      .on('tick', ticked);
+
+    this.simulation.force('link')
+      .links(graph.edges);
   }
 }
